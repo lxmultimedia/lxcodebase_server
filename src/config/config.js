fd48e7429cbe3c53ca8e34f98f36d9b5a04d1214
Attilio Furci <attilio.furci@klaroweb.ch>
2020-03-10 15:31:17 +0100

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-	port: process.env.PORT || 8081,
+	port: process.env.PORT || 3306,
 	db: {
 		database: process.env.DB_NAME || 'af_test',
 		user: process.env.DB_USER || 'af_ltest',
 		password: process.env.DB_PASSWORD || '8$h8fl0U',
 		options: {
 			dialect: process.env.DIALECT || 'mysql',
-			host: process.env.HOST || 'http://verdi.sui-inter.net:3306',
+			host: process.env.HOST || 'verdi.sui-inter.net',
 			storage: './lxcodebase.sqlite'
 		}
 	},
