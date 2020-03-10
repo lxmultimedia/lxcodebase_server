0430c20fa70dbec61fa51e7ffb18b44c4a6c158e
Attilio Furci <attilio.furci@klaroweb.ch>
2020-03-10 15:14:23 +0100

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
 	port: process.env.PORT || 8081,
 	db: {
-		database: process.env.DB_NAME || 'vY2hrXxhMe',
-		user: process.env.DB_USER || 'vY2hrXxhMe',
-		password: process.env.DB_PASSWORD || 'TmvRwBytQm',
+		database: process.env.DB_NAME || 'af_ltest',
+		user: process.env.DB_USER || 'af_ltest',
+		password: process.env.DB_PASSWORD || '8$h8fl0U',
 		options: {
 			dialect: process.env.DIALECT || 'mysql',
-			host: process.env.HOST || 'remotemysql.com',
+			host: process.env.HOST || 'verdi.sui-inter.net',
 			storage: './lxcodebase.sqlite'
 		}
 	},
