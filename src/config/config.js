3ce3959d0bae0f052566fd97b2a68b3dd589dfdd
Attilio Furci <afurci@hosttech.ch>
2022-11-30 14:04:31 +0100

    cleanup

--- src/config/config.js
+++ src/config/config.js
@@ -1,16 +1,16 @@
 module.exports = {
   port: process.env.PORT || 8081,
   db: {
-    database: process.env.DB_NAME || 'lxbase_2021',
-    user: process.env.DB_USER || 'lxbase_1345',
-    password: process.env.DB_PASSWORD || 'kE66ho*9',
+    database: process.env.DB_NAME || "",
+    user: process.env.DB_USER || "",
+    password: process.env.DB_PASSWORD || "",
     options: {
-      dialect: process.env.DIALECT || 'mysql',
-      host: process.env.HOST || 'nerthus.ch-meta.net',
-      storage: './lxcodebase.sqlite'
-    }
+      dialect: process.env.DIALECT || "mysql",
+      host: process.env.HOST || "",
+      storage: "./lxcodebase.sqlite",
+    },
   },
   authentication: {
-    jwtSecret: process.env.JWT_SECRET || 'secret'
-  }
-}
+    jwtSecret: process.env.JWT_SECRET || "secret",
+  },
+};
