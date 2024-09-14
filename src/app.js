538bb7d7256add36aeeaf729ec29a373fb497a7c
lxm <lxmediacom@gmail.com>
2024-09-14 08:40:28 +0200

    dotenv support

--- src/app.js
+++ src/app.js
@@ -1,25 +1,22 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const morgan = require('morgan')
-const {sequelize} = require('./models')
-const config = require('./config/config')
+require("dotenv").config();
 
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const { sequelize } = require("./models");
+const config = require("./config/config");
 
-app.use(morgan('combined'))
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
 
-require('./routes')(app)
+app.use(morgan("combined"));
+app.use(bodyParser.json());
+app.use(cors());
 
+require("./routes")(app);
 
 //{force:true}
-sequelize.sync()
-  .then(() => {
-
-	app.listen(config.port)
-	console.log(`Server started on port ${config.port}`)
-})
-
-
+sequelize.sync().then(() => {
+  app.listen(config.port);
+  console.log(`Server started on port ${config.port}`);
+});
